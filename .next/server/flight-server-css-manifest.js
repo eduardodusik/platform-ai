89206e7b1e4c221198f654295fac50c2e8d8cc42
eduardo.dusik <eduardo.dusik@ifood.com.br>
2023-05-13 14:41:44 -0300

    feat: change folder structure

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,35 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/Users/eduardo.dusik/Documents/projects/personal/platform-ai/app/layout.tsx": [
+      "/Users/eduardo.dusik/Documents/projects/personal/platform-ai/app/globals.css",
+      "/Users/eduardo.dusik/Documents/projects/personal/platform-ai/node_modules/.pnpm/next@13.4.2_react-dom@18.2.0_react@18.2.0/node_modules/next/font/local/target.css?{\"path\":\"app/fonts/index.ts\",\"import\":\"\",\"arguments\":[{\"src\":\"./SF-Pro-Display-Medium.otf\",\"variable\":\"--font-sf\"}],\"variableName\":\"sfPro\"}",
+      "/Users/eduardo.dusik/Documents/projects/personal/platform-ai/node_modules/.pnpm/next@13.4.2_react-dom@18.2.0_react@18.2.0/node_modules/next/font/google/target.css?{\"path\":\"app/fonts/index.ts\",\"import\":\"Inter\",\"arguments\":[{\"variable\":\"--font-inter\",\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "/Users/eduardo.dusik/Documents/projects/personal/platform-ai/app/dashboard/layout.tsx": [
+      "/Users/eduardo.dusik/Documents/projects/personal/platform-ai/components/shared/icons/loading-dots.module.css",
+      "/Users/eduardo.dusik/Documents/projects/personal/platform-ai/components/shared/icons/loading-spinner.module.css"
+    ],
+    "/Users/eduardo.dusik/Documents/projects/personal/platform-ai/app/project/page.tsx": [
+      "/Users/eduardo.dusik/Documents/projects/personal/platform-ai/node_modules/.pnpm/reactflow@11.7.0_immer@10.0.2_react-dom@18.2.0_react@18.2.0/node_modules/reactflow/dist/style.css"
+    ]
+  },
+  "cssModules": {
+    "/Users/eduardo.dusik/Documents/projects/personal/platform-ai/app/page": [
+      "/Users/eduardo.dusik/Documents/projects/personal/platform-ai/app/globals.css",
+      "/Users/eduardo.dusik/Documents/projects/personal/platform-ai/node_modules/.pnpm/next@13.4.2_react-dom@18.2.0_react@18.2.0/node_modules/next/font/local/target.css?{\"path\":\"app/fonts/index.ts\",\"import\":\"\",\"arguments\":[{\"src\":\"./SF-Pro-Display-Medium.otf\",\"variable\":\"--font-sf\"}],\"variableName\":\"sfPro\"}",
+      "/Users/eduardo.dusik/Documents/projects/personal/platform-ai/node_modules/.pnpm/next@13.4.2_react-dom@18.2.0_react@18.2.0/node_modules/next/font/google/target.css?{\"path\":\"app/fonts/index.ts\",\"import\":\"Inter\",\"arguments\":[{\"variable\":\"--font-inter\",\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "/Users/eduardo.dusik/Documents/projects/personal/platform-ai/app/dashboard/page": [
+      "/Users/eduardo.dusik/Documents/projects/personal/platform-ai/app/globals.css",
+      "/Users/eduardo.dusik/Documents/projects/personal/platform-ai/node_modules/.pnpm/next@13.4.2_react-dom@18.2.0_react@18.2.0/node_modules/next/font/local/target.css?{\"path\":\"app/fonts/index.ts\",\"import\":\"\",\"arguments\":[{\"src\":\"./SF-Pro-Display-Medium.otf\",\"variable\":\"--font-sf\"}],\"variableName\":\"sfPro\"}",
+      "/Users/eduardo.dusik/Documents/projects/personal/platform-ai/node_modules/.pnpm/next@13.4.2_react-dom@18.2.0_react@18.2.0/node_modules/next/font/google/target.css?{\"path\":\"app/fonts/index.ts\",\"import\":\"Inter\",\"arguments\":[{\"variable\":\"--font-inter\",\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/Users/eduardo.dusik/Documents/projects/personal/platform-ai/components/shared/icons/loading-dots.module.css",
+      "/Users/eduardo.dusik/Documents/projects/personal/platform-ai/components/shared/icons/loading-spinner.module.css"
+    ],
+    "/Users/eduardo.dusik/Documents/projects/personal/platform-ai/app/project/page": [
+      "/Users/eduardo.dusik/Documents/projects/personal/platform-ai/app/globals.css",
+      "/Users/eduardo.dusik/Documents/projects/personal/platform-ai/node_modules/.pnpm/next@13.4.2_react-dom@18.2.0_react@18.2.0/node_modules/next/font/local/target.css?{\"path\":\"app/fonts/index.ts\",\"import\":\"\",\"arguments\":[{\"src\":\"./SF-Pro-Display-Medium.otf\",\"variable\":\"--font-sf\"}],\"variableName\":\"sfPro\"}",
+      "/Users/eduardo.dusik/Documents/projects/personal/platform-ai/node_modules/.pnpm/next@13.4.2_react-dom@18.2.0_react@18.2.0/node_modules/next/font/google/target.css?{\"path\":\"app/fonts/index.ts\",\"import\":\"Inter\",\"arguments\":[{\"variable\":\"--font-inter\",\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}
