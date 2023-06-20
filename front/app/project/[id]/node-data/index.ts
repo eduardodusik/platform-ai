b205106e3fa5fa1f5cf14118ca992e6d73b62ce4
eduardo.dusik <eduardo.dusik@ifood.com.br>
2023-06-20 19:58:57 -0300

    feat: adjust live cursosr

--- front/app/project/[id]/node-data/index.ts
+++ front/app/project/[id]/node-data/index.ts
@@ -4,7 +4,7 @@ import { START_CONFIG } from "@/app/project/[id]/node-data/start";
 import { NodeDataBase } from "@/components/project/nodes/customNodeTypes";
 import { WHISPER_CONFIG } from "@/app/project/[id]/node-data/whisper";
 
-export const AvailableNodes: Record<NODE_IDS_ENUM, NodeDataBase> = {
+export const AvailableNodes: Record<NODE_IDS_ENUM, any> = {
   [NODE_IDS_ENUM.GPT]: GPT_CONFIG,
   [NODE_IDS_ENUM.START]: START_CONFIG,
   [NODE_IDS_ENUM.WHISPER]: WHISPER_CONFIG,
